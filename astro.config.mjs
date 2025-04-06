87d4941d076f5871d1eb9e03c2fc8b4916b21d37
Glauber Botelho <glauber.a.botelho@gmail.com>
2025-04-06 10:38:29 -0300

    finishing tutorial unit 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://admirable-licorice-da9627.netlify.app/"
+  site: "https://admirable-licorice-da9627.netlify.app/",
+  integrations: [preact()]
 });
